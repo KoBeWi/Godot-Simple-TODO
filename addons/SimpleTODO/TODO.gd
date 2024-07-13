b94914419513cce86278fc1cfdbe152fd17fe864
kobewi <kobewi4e@gmail.com>
2024-07-13 19:30:04 +0200

    Fix drag error

--- addons/SimpleTODO/TODO.gd
+++ addons/SimpleTODO/TODO.gd
@@ -91,5 +91,9 @@ func update_full_counter():
 	_update_full_counter.call_deferred()
 
 func _update_full_counter():
-	%Total.text = str("Total: %d" % column_container.get_children().reduce(func(accum: int, column: Node) -> int: return accum + column.item_container.get_child_count(), 0))
+	%Total.text = str("Total: %d" % column_container.get_children().reduce(func(accum: int, column: Node) -> int:
+		if &"item_container" in column:
+			return accum + column.item_container.get_child_count()
+		else:
+			return accum, 0))
 	counter_queued = false
